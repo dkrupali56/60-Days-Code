4d904f1199c9520b30006180cc673b160c94f51c
DESAI KRUPALI RAJESHBHAI <102245210+dkrupali56@users.noreply.github.com>
2023-03-25 07:50:01 +0530

    Add files via upload

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,18 +1,25 @@
-// API Key for OpenWeatherMap API
-const API_KEY = 'YOUR_API_KEY_HERE';
+const searchButton = document.getElementById("search-btn");
+const searchInput = document.querySelector("input[type='text']");
+const locationElement = document.getElementById("location");
+const weatherIconElement = document.getElementById("weather-icon");
+const temperatureElement = document.getElementById("temperature");
+const descriptionElement = document.getElementById("description");
 
-// Get HTML elements
-const locationInput = document.getElementById('location');
-const submitButton = document.getElementById('submit');
-const currentWeather = document.getElementById('current-weather');
-const weatherIcon = document.getElementById('weather-icon');
-const weatherDetails = document.getElementById('weather-details');
-const forecastDetails = document.getElementById('forecast-details');
+searchButton.addEventListener("click", () => {
+  const location = searchInput.value;
 
-// Event listener for form submission
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  // Get location input value
-  const location = locationInput.value;
-  // Call API to get weather data
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=YOUR_API_KEY&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+      locationElement.textContent = data.name;
+      weatherIconElement.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}">`;
+      temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+      descriptionElement.textContent = data.weather[0].description;
+    })
+    .catch(error => {
+      locationElement.textContent = "";
+      weatherIconElement.innerHTML = "";
+      temperatureElement.textContent = "";
+      descriptionElement.textContent = "Unable to retrieve weather data. Please try again.";
+    });
+});
