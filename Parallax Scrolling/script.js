4b07170897476c7829e1cdaa210b493f77a547ed
DESAI KRUPALI RAJESHBHAI <102245210+dkrupali56@users.noreply.github.com>
2023-04-25 08:04:19 +0530

    Add files via upload

---
+++ Parallax Scrolling/script.js
@@ -0,0 +1,6 @@
+// Add the parallax effect to the header and footer elements
+const header = document.querySelector('.header');
+header.classList.add('parallax');
+
+const footer = document.querySelector('.footer');
+footer.classList.add('parallax');
